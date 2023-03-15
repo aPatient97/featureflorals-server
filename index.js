79a2070e67fbcec0e501be6c2a3c3815f8ef14d7
Alex Patient <apatient97@gmail.com>
2023-03-15 16:35:15 +0000

    added delivery option

--- index.js
+++ index.js
@@ -44,13 +44,6 @@ app.post("/checkout", async (req, res) => {
           {
             shipping_rate_data: {
               type: 'fixed_amount',
-              fixed_amount: {amount: 600, currency: 'gbp'},
-              display_name: 'Sunday 18th March delivery',
-              delivery_estimate: {
-                minimum: {unit: 'business_day', value: 1},
-                maximum: {unit: 'business_day', value: 5},
-              },
-              type: 'fixed_amount',
               fixed_amount: {amount: 0, currency: 'gbp'},
               display_name: 'Free collection on Saturday 17th March between 12pm-8pm',
               delivery_estimate: {
@@ -58,6 +51,15 @@ app.post("/checkout", async (req, res) => {
                 maximum: {unit: 'business_day', value: 5},
               }
             },
+            shipping_rate_data: {
+              type: 'fixed_amount',
+              fixed_amount: {amount: 600, currency: 'gbp'},
+              display_name: 'Sunday 18th March delivery',
+              delivery_estimate: {
+                minimum: {unit: 'business_day', value: 1},
+                maximum: {unit: 'business_day', value: 5},
+              },
+            }
           },
         ],
         line_items: lineItems,
