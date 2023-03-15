0b8bbf75aa76b005126929debc6a097a6abfefd9
Alex Patient <apatient97@gmail.com>
2023-03-15 18:04:55 +0000

    cancel webhook

--- index.js
+++ index.js
@@ -64,55 +64,55 @@ app.post("/checkout", async (req, res) => {
     }));
 });
 
-const endpointSecret = "whsec_5ca578455e29aa1aa42df8a5894dafc5335ea3d425e6aae9ce872ecf9aac722b";
+// const endpointSecret = "whsec_5ca578455e29aa1aa42df8a5894dafc5335ea3d425e6aae9ce872ecf9aac722b";
 
-app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-    const sig = request.headers['stripe-signature'];
+// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+//     const sig = request.headers['stripe-signature'];
   
-    let event;
+//     let event;
   
-    try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
+//     try {
+//       event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+//     } catch (err) {
+//       response.status(400).send(`Webhook Error: ${err.message}`);
+//       return;
+//     }
   
-    // Handle the event
-    switch (event.type) {
-        case 'billing_portal.session.created':
-            const billingPortalSessionCreated = event.data.object;
-            // Then define and call a function to handle the event billing_portal.session.created
-            console.log(`${event.type} has been logged yee boiii`)
-            break;
-          case 'checkout.session.completed':
-            const checkoutSessionCompleted = event.data.object;
-            // Then define and call a function to handle the event checkout.session.completed
-            break;
-          case 'payment_intent.canceled':
-            const paymentIntentCanceled = event.data.object;
-            // Then define and call a function to handle the event payment_intent.canceled
-            break;
-          case 'payment_intent.created':
-            const paymentIntentCreated = event.data.object;
-            // Then define and call a function to handle the event payment_intent.created
-            break;
-          case 'payment_intent.payment_failed':
-            const paymentIntentPaymentFailed = event.data.object;
-            // Then define and call a function to handle the event payment_intent.payment_failed
-            break;
-          case 'payment_intent.succeeded':
-            const paymentIntentSucceeded = event.data.object;
-            // Then define and call a function to handle the event payment_intent.succeeded
-            break;
-          // ... handle other event types
-          default:
-            console.log(`Unhandled event type ${event.type}`);
-        }
+//     // Handle the event
+//     switch (event.type) {
+//         case 'billing_portal.session.created':
+//             const billingPortalSessionCreated = event.data.object;
+//             // Then define and call a function to handle the event billing_portal.session.created
+//             console.log(`${event.type} has been logged yee boiii`)
+//             break;
+//           case 'checkout.session.completed':
+//             const checkoutSessionCompleted = event.data.object;
+//             // Then define and call a function to handle the event checkout.session.completed
+//             break;
+//           case 'payment_intent.canceled':
+//             const paymentIntentCanceled = event.data.object;
+//             // Then define and call a function to handle the event payment_intent.canceled
+//             break;
+//           case 'payment_intent.created':
+//             const paymentIntentCreated = event.data.object;
+//             // Then define and call a function to handle the event payment_intent.created
+//             break;
+//           case 'payment_intent.payment_failed':
+//             const paymentIntentPaymentFailed = event.data.object;
+//             // Then define and call a function to handle the event payment_intent.payment_failed
+//             break;
+//           case 'payment_intent.succeeded':
+//             const paymentIntentSucceeded = event.data.object;
+//             // Then define and call a function to handle the event payment_intent.succeeded
+//             break;
+//           // ... handle other event types
+//           default:
+//             console.log(`Unhandled event type ${event.type}`);
+//         }
   
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
-  });
+//     // Return a 200 response to acknowledge receipt of the event
+//     response.send();
+//   });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
